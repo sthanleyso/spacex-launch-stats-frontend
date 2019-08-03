343ff6f209be62e360eb785502d840d4d21eed14
sthanleyso <sthanleys7@gmail.com>
2019-08-03 15:33:32 -0300

    Updates

---
+++ precache-manifest.ae70609606fbec743e92cdfa1fb11326.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31816d9b76355fc59ddfff879eac81b6",
+    "url": "/spacex-launch-stats-frontend/index.html"
+  },
+  {
+    "revision": "404e7d1dcacc5ede99c3",
+    "url": "/spacex-launch-stats-frontend/static/js/2.8a78b019.chunk.js"
+  },
+  {
+    "revision": "acaedf572a5e9a904be8",
+    "url": "/spacex-launch-stats-frontend/static/js/main.fdf1a1a4.chunk.js"
+  },
+  {
+    "revision": "8b685c18b788d2c1e2c7",
+    "url": "/spacex-launch-stats-frontend/static/js/runtime~main.310d0c1e.js"
+  },
+  {
+    "revision": "950ba66541181838b6c07f14d62e331a",
+    "url": "/spacex-launch-stats-frontend/static/media/spacex-logo.950ba665.png"
+  }
+]);
